df78a70fbf94e6a286a8bbdfd086a659dc4b27c8
Ben Williams <benboi294@gmail.com>
2025-03-07 11:26:55 +0000

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 app = FastAPI()
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = "sqlite:///home/benboi8/plush.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
